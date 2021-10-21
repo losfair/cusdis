4e4669877f66c2ac6ab1fd5bddde1e0835e7dd81
losfair <zhy20000919@hotmail.com>
2021-10-21 14:31:13 +0000

    fix

--- pages/api/auth_error.ts
+++ pages/api/auth_error.ts
@@ -6,7 +6,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   if(!resolvedConfig.externalJwtAuthRedirectUrlParam) return res.status(400).json({error: "no redirect url param"});
   if(!req.headers.host) return res.status(400).json({error: "missing host header"});
 
-  const callback = "https://" + req.headers.host + req.url;
+  const callback = "https://" + req.headers.host + "/api/auth/signin";
   const u = new URL(resolvedConfig.externalJwtAuthRedirectUrl);
   u.searchParams.set(resolvedConfig.externalJwtAuthRedirectUrlParam, callback);
   res.redirect(302, u.toString());
